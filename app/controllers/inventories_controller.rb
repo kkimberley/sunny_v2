45cbaef102543192ac6d6bca52a88681355eda66
kim <kim@kimde-MBP.fang>
2019-05-17 21:52:56 +0800

    add delete method

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -41,6 +41,13 @@ class InventoriesController < ApplicationController
                          end
   end
 
+  def destroy
+    inventory = Inventory.find params[:id]
+    inventory.destroy
+
+    redirect_to inventories_path
+  end
+
   private
 
   def inventory_params
